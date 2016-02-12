4055e6c7fcbf12aa6cf3ff1fa563569a96673648
Stephen Kempin <info@stephenkempin.co.uk>
2016-02-12 13:02:44 +0000

    Updating plugin name to FUS

---
+++ includes/fus-i18n.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Define the internationalization functionality
+ *
+ * Loads and defines the internationalization files for this plugin
+ * so that it is ready for translation.
+ *
+ * @link       http://example.com
+ * @since      1.0.0
+ *
+ * @package    Facebook-URL-Stats
+ * @subpackage Facebook-URL-Stats/includes
+ */
+
+class Plugin_Name_i18n {
+
+
+	/**
+	 * Load the plugin text domain for translation.
+	 *
+	 * @since    1.0.0
+	 */
+	public function load_plugin_textdomain() {
+
+		load_plugin_textdomain(
+			'plugin-name',
+			false,
+			dirname( dirname( plugin_basename( __FILE__ ) ) ) . '/languages/'
+		);
+
+	}
+
+
+
+}
